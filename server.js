c9b48339363602249cc5d442e87268f133580d8c
Kiaan <lightlabcreation@gmail.com>
2026-06-18 19:56:08 +0530

    update

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    // "https://sarnic-new.netlify.app/",
 
+  ],
+  credentials: true
+}));
 // Initialize Socket.IO
 initSocket(server);
 
