08291e1ef7bbb71658dc709e5065fa5d74290a06
ruchitamrakar194-eng <ruchitamrakar194@gmail.com>
2026-06-19 11:52:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
+app.options(/.*/, cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
